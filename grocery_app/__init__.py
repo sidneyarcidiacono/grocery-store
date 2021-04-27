b89ca6d2e6798571261b7cc43625783db3db4d8a
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-27 11:56:48 -0700

    [update] Finalizing packages refactor, shopping list.

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from grocery_app.config import Config
 import os
 
@@ -9,13 +9,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = os.urandom(24)
 
+
 db = SQLAlchemy(app)
-
-login_manager = LoginManager()
+login_manager = LoginManager(app)
 login_manager.login_view = "auth.login"
-login_manager.init_app(app)
+bcrypt = Bcrypt(app)
 
-from .models import User
+from grocery_app.models import User
 
 
 @login_manager.user_loader
@@ -23,14 +23,12 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-bcrypt = Bcrypt(app)
-
-
-from grocery_app.routes import main
-from grocery_app.routes import auth
+from grocery_app.auth.routes import auth
+from grocery_app.main.routes import main
 
 app.register_blueprint(main)
 app.register_blueprint(auth)
+
 
 with app.app_context():
     db.create_all()
